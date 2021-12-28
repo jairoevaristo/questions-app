154182e8e291f8ca3f3204d081cb7ec18f0ce4c8
jairo.evaristo <jairo.evaristo@osf.digital>
2021-12-27 22:51:18 -0300

    ajuste de responsividade em desktop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { Emitter } from './lib/Emitter.js';
-import { Timer } from './time/index.js';
+import { Emitter } from "./lib/Emitter.js";
+import { Timer } from "./time/index.js";
 
 const App = {
   init() {
     Timer.start(0.5);
-
-    Emitter.on('time-end', Timer.start);
-  }
-}
+    Emitter.on("time-end", Timer.start);
+  },
+};
 
 export { App };
